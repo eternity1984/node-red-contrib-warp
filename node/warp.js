f44c5559601608ee35824897e4a50f2333c0be98
yu_inoue <eternity.1984.10.04@gmail.com>
2021-04-07 23:25:44 +0900

    fix

--- node/warp.js
+++ node/warp.js
@@ -4,33 +4,45 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         var node = this;
         node.on('input', function(msg, _send, done) {
-            var scope = msg.scope || config.scope;
-            if (!Array.isArray(scope)) {
-                scope = [scope];
+
+            var scope = config.scope || [];
+            if (config.destination === 'varmsg') {
+                const msgVal = RED.util.getMessageProperty(msg, 'scope');
+
+                if (typeof msgVal === 'string') {
+                    // a comma-separated string
+                    const re = /\s*(?:,|$)\s*/;
+                    scope = msgVal.split(re);
+                } else if (Array.isArray(msgVal)) {
+                    // an array
+                    scope = msgVal.filter(x => typeof x === 'string');
+                } else {
+                    const err = new TypeError("`msg.scope` must be an array or a comma-separated string.");
+                    if (done) {
+                        done(err);
+                    } else {
+                        node.error(err, msg);   // Node-RED 0.x compatible
+                    }
+                    return;
+                }
             }
-            scope = [...new Set(scope)];
+            scope = [...new Set(scope)].filter(x => x);
 
             var missing = [];
             scope.forEach(id => {
-                const target = node._flow.getNode(id);
-                if (target) {
-                    target.receive(RED.util.cloneMessage(msg));
+                const nodeRef = node._flow.getNode(id); // RED.nodes.getNode(id);
+                if (nodeRef && (nodeRef.type !== 'warp')) {
+                    nodeRef.receive(RED.util.cloneMessage(msg));
                 } else {
                     missing.push(id);
                 }
             });
 
             if (missing.length > 0) {
-                const err = "Missing scope: [" + missing + "]";
-                if (done) {
-                    done(err);
-                } else {
-                    node.error(err, msg);
-                }
-            } else {
-                if (done) {
-                    done();
-                }
+                node.warn('Invalid target: [' + missing + ']');
+            }
+            if (done) {
+                done();
             }
         });
     }
